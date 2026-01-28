21bb99a60ce195b70816c10618079d1305deaf72
JOSHUA MUJAKARI <joshuamujakari15@gmail.com>
2026-01-27 16:21:48 -0800

    Fix Service Worker error by clearing caches

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,16 @@
-const CACHE_NAME = 'studysync-v1';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/manifest.json'
-];
+const CACHE_NAME = 'studysync-cleanup-v1';
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => caches.delete(cache))
+            );
         })
     );
+    self.clients.claim();
 });
